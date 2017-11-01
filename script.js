b1703fb04d6be67cf05e9ed84aea276831078dab
tkat7 <tkatona2013@gmail.com>
2017-11-01 14:14:01 -0400

    HTML structure and some JS

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ $(document).ready(function() {
 
 
 });
+
+
+function showHome() {
+$(".home").show();
+$(".shoppe").hide();
+$(".cart").hide();
+}
+
+function showShoppe() {
+  $(".home").hide();
+  $(".shoppe").show();
+  $(".cart").hide();
+}
+
+function showCart() {
+  $(".home").hide();
+  $(".shoppe").hide();
+  $(".cart").show();
+}
+
+function popup(shirt) {
+
+}
+
+function exitPopup() {
+
+}
