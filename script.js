ad159a641b3362760f9dc08092cf401642d807ae
tkat7 <tkatona2013@gmail.com>
2017-11-03 09:42:47 -0400

    comments

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var subtotal = 0;
 var total = 0;
 
+//hides the shop
+//Shows the cart and displays all items in the array with a  quantity over 0
+//and sets the description quantity and price on those items to what they are in the array
 function showCart() {
   $("#shoppe").hide();
   $("#cart").show();
@@ -16,6 +19,8 @@ if (items[count].quantity > 0) {
   });
 }
 
+//unhides the item popup in our shop
+//sets the popup image and price from the array
 function popup(item) {
 var element = items[item];
 $("#shoppePop").show();
@@ -24,11 +29,13 @@ $("#item").attr("src", element.src);
 $("#hidden").val(item);
 }
 
+//shows the shop and hides the cart
 function showShoppe() {
   $("#shoppe").show();
   $("#cart").hide();
 }
 
+//This is our array of items in our shop
 var items = [{
   name: "none",
   category: "none",
@@ -134,6 +141,8 @@ var items = [{
   src: "syntaxImages/redhoodie.jpg"
 }];
 
+//loops through the array and calculates the subtotal and total
+//displays the payment popup
 function checkout() {
   $.each(items, function(item){
     subtotal += parseInt(items[item].price * items[item].quantity);
@@ -142,23 +151,30 @@ function checkout() {
   $("#payment").show();
 }
 
+//closes the card or credit popup
+//shows the receipt popup
 function pay(style){
-
 close(style);
 $("#receipt").show();
 }
 
+//hides the payment popup
+//shows the card popup
 function showCard(){
 $("#creditOptions").show();
 close("payment");
 }
 
+//hides the payment popup
+//shows the cash popup
 function showCash(){
   $("#total").html("Total: " + total);
 $("#cashMoney").show();
 close("payment");
 }
 
+//gets the item that was clicked and the ammount added
+//changes the quantity in the array
 function addToCart() {
 var item = $("#hidden").val();
 var element = items[item];
@@ -166,13 +182,18 @@ element.quantity += parseInt($("#quantity").val());
 close("shoppePop");
 }
 
+//closes whatever popup
 function close(popup) {
 $("#" + popup).hide();
 }
+
+//this calculates change
 $("#amountTendered").change(function() {
 var remainder = ($("#amountTendered").val() - total);
 $("#change").html("Change: " + remainder.toFixed(2));
 });
+
+//this updates the custom word after every keystoke
 $(document).ready(function() {
 $("#text").keyup(function(){
   var text = $("#text").val();
