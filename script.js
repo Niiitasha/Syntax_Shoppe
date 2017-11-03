c18e8f6168f267a3b9def28f446989096c529787
tkat7 <tkatona2013@gmail.com>
2017-11-02 20:56:45 -0400

    NO MORE ALERTIMG NE

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function showCart() {
-  alert("showCart");
   $("#shoppe").hide();
   $("#cart").show();
 }
@@ -13,7 +12,6 @@ $("#hidden").val(item);
 }
 
 function showShoppe() {
-  alert('showShoppe');
   $("#shoppe").show();
   $("#cart").hide();
 }
